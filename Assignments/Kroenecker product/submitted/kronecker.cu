2b531d1ee71fa48f7aadc87b97085ab87256da7e
ahdhn <eng.a.hassen@gmail.com>
2018-02-11 21:25:01 -0800

    resubmitted

--- Assignments/Kroenecker product/submitted/kronecker.cu
+++ Assignments/Kroenecker product/submitted/kronecker.cu
@@ -72,6 +72,81 @@ void checkResults(int N, int M, float* gpu_result, float* cpu_result){
 /***************************************************/
 __global__ void KroneckerKernel(const int M, const int N, float* A, float* B, float* C){
 
+	extern __shared__ float shrd_ptr[];
+	float*sh_B = shrd_ptr;
+	__shared__ float shrd_C[1024];
+	__shared__ uint32_t shrd_C_id[1024];
+
+	uint32_t tid = threadIdx.x;
+	uint32_t sh_b_size = (N*N <= 64*64) ? N*N: 64*64;
+	int b_jump = -sh_b_size;
+
+
+	uint32_t numJumps = max(1,((M*M) / (blockDim.x * gridDim.x))); // at least do one run
+
+
+	for (uint32_t jump_id = 0; jump_id < numJumps; jump_id++){ //how many different groups of elements a block will touch
+		                                                        //groups of elements are elemnts next to one another in A
+
+
+		uint32_t A_id = blockIdx.x * blockDim.x + tid + jump_id*gridDim.x*blockDim.x; //the element A id
+
+		if(A_id < M*M){
+
+			uint32_t A_i = A_id / M;
+			uint32_t A_j = A_id % M;
+			float A_ele = A[A_id];
+
+			int id =0;
+
+#pragma unroll
+			for (uint32_t B_id = 0; B_id < N*N; B_id++){
+
+				if(B_id%sh_b_size == 0 ){
+					__syncthreads();
+					b_jump += sh_b_size;
+					for (uint32_t i = tid; i < sh_b_size; i += blockDim.x){
+						sh_B[i] = B[i+b_jump];
+					}
+					__syncthreads();
+				}
+
+
+				float B_ele;
+				B_ele = sh_B[B_id-b_jump];
+
+
+				uint32_t B_i = B_id / N;
+				uint32_t B_j = B_id % N;
+				uint32_t C_i = A_i*N + B_i;
+				uint32_t C_j = A_j*N + B_j;
+				uint32_t C_id = C_i*N*M + C_j;
+				//C[C_id] = A_ele*B_ele;
+
+				shrd_C[tid*blockDim.x + id] = A_ele*B_ele;
+				shrd_C_id[tid*blockDim.x + id] = C_id;
+				id++;
+
+				if(id == 32){
+					__syncthreads();
+#pragma unroll
+					for (int i=0; i<1024; i+=32){
+						C[shrd_C_id[tid+i]] = shrd_C[tid+i];
+					}
+					id = 0;
+					__syncthreads();
+				}
+
+
+
+
+
+
+
+			}
+
+		}
+	}
 
 }
 
@@ -237,8 +312,8 @@ void KroneckerGPUSmall(int M, int N, float* A, float* B, float*C){
 
 
 
-
-/*int main(int argc, char* argv[]) {
+/*
+int main(int argc, char* argv[]) {
 
 	if (argc != 3){
 		std::cout << "\n	Usage ./kron [M][N]" << std::endl;
@@ -320,7 +395,7 @@ void KroneckerGPUSmall(int M, int N, float* A, float* B, float*C){
 		B[i] = float(i + 1);
 	}
 
-	KroneckerGPUSmall(M, N, A, B, gpu_result);
+	KroneckerGPU(M, N, A, B, gpu_result);
 	KroneckerCPU(M, N, A, B, cpu_result);
 
 	checkResults(N, M, gpu_result, cpu_result);
@@ -339,58 +414,76 @@ void KroneckerGPUSmall(int M, int N, float* A, float* B, float*C){
 //****************************************************************************//
 void KroneckerGPU(int M, int N, float* A, float* B, float* C) {
 
+	//The assumtion here is that B is at most 16kB
+	//Prefer more L1-cache because we at max need 16kB shared memory
+	//cudaFuncSetCacheConfig(KroneckerKernelSmall, cudaFuncCachePreferL1);
+	cudaFuncSetCacheConfig(KroneckerKernelSmall, cudaFuncCachePreferShared);
+
 	float *d_A, *d_B, *d_C;
 	const int M2 = M*M;
 	const int N2 = N*N;
-	//allocate memory on device
+
+	//****allocate memory on device***//
 	HANDLE_ERROR(cudaMallocManaged(&d_A, M2*sizeof(float)));
 	HANDLE_ERROR(cudaMallocManaged(&d_B, N2*sizeof(float)));
 	HANDLE_ERROR(cudaMallocManaged(&d_C, M2*N2*sizeof(float)));
 
-#define my_debug
+//#define my_debug
 
-	//move data to device
+	//****move data to device***//
 	for (int i = 0; i<N2; i++){ d_B[i] = B[i]; }
 	for (int i = 0; i<M2; i++){ d_A[i] = A[i]; }
 
 
-	//calc gird and block size
-	const int numBlocks = 4096;
-	const int numThreads = 256;
-
-
 #ifdef my_debug
-	const int numIter = 1000;
+
+
+	const int shrdMem = (N*N <= 64*64) ? N*N: 64*64;//amount of shared memory is the size of B (we move it all in shared memory)
+
+
+	const int numIter = 1;
 	const float mem = float((sizeof(float)*(N2 + M2 + M2*N2)) / (1024.0f * 1024.0f)); //in mega bytes
+
+
+	int numBlocks = 1024;
+	int numThreads = 32;
+
 	cudaEvent_t start, stop;
 	HANDLE_ERROR(cudaEventCreate(&start));//timing
 	HANDLE_ERROR(cudaEventCreate(&stop));
-	HANDLE_ERROR(cudaEventRecord(start, NULL));
+	HANDLE_ERROR(cudaEventRecord(start, 0));
 	for (int i = 0; i<numIter; i++){
-#endif
-		KroneckerKernelSmall << < numBlocks, numThreads >> >(M, N, A, B, C);
-#ifdef my_debug
+		KroneckerKernel <<< numBlocks, numThreads, shrdMem*sizeof(float) >>>(M, N, d_A, d_B, d_C);
 	}
-#endif
-
-	cudaDeviceSynchronize();
-
-#ifdef my_debug
-	HANDLE_ERROR(cudaEventRecord(stop, NULL));
+	HANDLE_ERROR(cudaDeviceSynchronize());
+	HANDLE_ERROR(cudaEventRecord(stop, 0));
 	HANDLE_ERROR(cudaEventSynchronize(stop));
 	float time = 0.0f;
 	HANDLE_ERROR(cudaEventElapsedTime(&time, start, stop));
 	time /= numIter; //time in mSec
 	time /= 1000;//time in seconds
 	float myBW = mem / time;
-	std::cout << "\n  numThreads= " << numThreads << " numBlocks= " << numBlocks << " time(sec)= " << time << "  BW(MB)=" << myBW << "   BW_frac(MB/sec)= " << myBW / maxBW << std::endl;
+	float gflops = (float(M2)*float(N2))/(1024.0f*1024.0f*1024.0f);
+	std::cout << "\n M= "<<M << " N= "<< N <<" numThreads= " << numThreads << " numBlocks= " << numBlocks << " time(sec)= " << time << " BW(MB/s)= " << myBW
+	<< " BW_prec= " << 100*(myBW / maxBW)<<" GFLOPs/Sec= "<< gflops/time  << std::endl;
+
+#else
+	const int shrdMem = (N*N <= 64*64) ? N*N: 64*64;//amount of shared memory is the size of B (we move it all in shared memory)
+
+	int numBlocks = 2048;
+	int numThreads = 32;
+
+	KroneckerKernel <<< numBlocks, numThreads, shrdMem*sizeof(float) >>>(M, N, d_A, d_B, d_C);
+	HANDLE_ERROR(cudaDeviceSynchronize());
+
 #endif
 
-	//move data to host
+	//****move data to host***//
 	for (int i = 0; i<N2*M2; i++){ C[i] = d_C[i]; }
 
 	cudaFree(d_A);
 	cudaFree(d_B);
 	cudaFree(d_C);
 
+	HANDLE_ERROR(cudaDeviceReset());
 }
