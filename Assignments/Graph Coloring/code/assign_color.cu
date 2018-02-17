b299c4d2239ffa0a5c4794bacd4fc986447d6a47
ahdhn <eng.a.hassen@gmail.com>
2018-02-17 13:45:11 -0800

    bug fix

--- Assignments/Graph Coloring/code/assign_color.cu
+++ Assignments/Graph Coloring/code/assign_color.cu
@@ -1,4 +1,4 @@
-__device __ __forceinline__ assign_color(uint32_t k,  uint32_t v, unsigned char* color){
+__device__ __forceinline__ void assign_color(uint32_t k,  uint32_t v, unsigned char* color){
 
 	//Assigne color k to vertex v
 }
---
+++ Assignments/Graph Coloring/code/indept_set.cu
@@ -0,0 +1,5 @@
+__device__ __forceinline__ void indept_set(uint32_t NumRow, uint32_t numNNZ, uint32_t *col_id, uint32_t *offset){
+
+	//Create independent set 
+
+}
