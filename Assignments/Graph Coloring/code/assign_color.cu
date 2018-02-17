edf92645f4a180289ee51a230d5238ace62285fc
ahdhn <eng.a.hassen@gmail.com>
2018-02-16 19:38:16 -0800

    placeholder kernels

---
+++ Assignments/Graph Coloring/code/assign_color.cu
@@ -0,0 +1,4 @@
+__device __ __forceinline__ assign_color(uint32_t k,  uint32_t v, unsigned char* color){
+
+	//Assigne color k to vertex v
+}
--- Assignments/Graph Coloring/code/coloring.cu
+++ Assignments/Graph Coloring/code/coloring.cu
@@ -1,3 +1,6 @@
+#include "assign_color.cu"
+#include "indept_set.cu"
+
 __global__ void graphColoring(uint32_t NumRow, //number of vertices (= number of rows in adjacency matrix)
 	                          uint32_t numNNZ, //number of non zero entry of the adjacency matrix
 	                          uint32_t *col_id, //the column id in the CSR format 
@@ -5,7 +8,7 @@ __global__ void graphColoring(uint32_t NumRow, //number of vertices (= number of
 	                          unsigned char* color //the color of the vertices (output)
 	                          ){
 
-	
+
 
 
 }
