1ce218aaeb55654be18df9ca79907b19d31ee499
YuxinxinChen <yxxchen@ucdavis.edu>
2018-02-20 23:34:07 -0800

    works on dumb example

--- Assignments/Graph Coloring/code/conflict_resolve_forgetabout_sharedmemory.cu
+++ Assignments/Graph Coloring/code/conflict_resolve_forgetabout_sharedmemory.cu
@@ -26,10 +26,8 @@ void FindChangeColor(uint32_t *changeColor, uint32_t sizeNode, uint32_t *nodes, 
 
         int neighbor=-1;
 	int neighborOwner = -1;
-	printf("sizeNeighLen: %d\n", sizeNeighLen);
         for(int i=blockIdx.x*blockDim.x+threadIdx.x; i<sizeNeighLen; i+=blockDim.x*gridDim.x)
         {
-		printf("I am thread%d\n", i);
                 // calculate who to fetch
                 for(int j=0; j<sizeNode; j++)
                 {
@@ -37,12 +35,12 @@ void FindChangeColor(uint32_t *changeColor, uint32_t sizeNode, uint32_t *nodes, 
                         {
                                 //j's neighborlist
                                 neighborOwner = j;
-                                printf("I am thread%d, my neighberOwner=%d nodes[%d]=%d neighbor=%d\n", threadIdx.x, neighborOwner,j,nodes[j], neighbor);
 				break;
                         }
                 }
 
 		neighbor = tr_col_id[tr_offset[nodes[neighborOwner]]+(i-neighLen[neighborOwner])];
+                                printf("I am thread%d, my neighberOwner=%d nodes[%d]=%d neighbor=%d\n", threadIdx.x, neighborOwner,neighborOwner,nodes[neighborOwner], neighbor);
 		for(int j=0; j<sizeNode; j++)
 		{
 			if(neighbor == nodes[j])
@@ -50,6 +48,12 @@ void FindChangeColor(uint32_t *changeColor, uint32_t sizeNode, uint32_t *nodes, 
 				changeColor[neighborOwner]=1;
 				break;
 			}
+		}
+		if(threadIdx.x==0)
+		{
+			for(int i=0; i<sizeNode; i++)
+				printf("changeColor[%d]=%d   ", i,changeColor[i] );
+			printf("\n");
 		}
         }
 }
@@ -65,12 +69,8 @@ void Conflict_assignColor(uint32_t *changeColor, int theColor, unsigned char *co
 
 
 __global__
-void GenNewNodes(uint32_t *nodes, uint32_t *newNodes, uint32_t *neighLen, uint32_t *newNeighLen, int &sizeNode, uint32_t *changeColor, int &check) {
-	if(changeColor[sizeNode]==0)
-	{
-		check = 1;
-		return;
-	}
+void GenNewNodes(uint32_t *nodes, uint32_t *newNodes, uint32_t *neighLen, uint32_t *newNeighLen, int sizeNode, uint32_t *changeColor) {
+
 	for(int i=blockIdx.x*blockDim.x+threadIdx.x; i<sizeNode; i+=blockDim.x*gridDim.x)
 	{
 		if(changeColor[i]+1 == changeColor[i+1])
@@ -79,8 +79,6 @@ void GenNewNodes(uint32_t *nodes, uint32_t *newNodes, uint32_t *neighLen, uint32
 			newNeighLen[changeColor[i]] = neighLen[i];
 		}
 	}
-	if(threadIdx.x==0 && blockIdx.x==0)
-		sizeNode = changeColor[sizeNode];
 }
 
 
@@ -120,11 +118,7 @@ void conflict_resolve_forgetabout_sharedmemory(uint32_t* conflict_color, // Arra
 	GetneighLen<<<1, 32>>>(nodes, sizeNode,  tr_offset, neighLen1, changeColor);
 	cudaDeviceSynchronize();
 
-	for(int i=0; i<sizeNode; i++)
-	{
-		std::cout<<"neighLen1["<<i<<"]= "<<neighLen1[i]<<"  ";
-	}
-	std::cout<<std::endl;
+
 
 	for(int i=0; i<sizeNode; i++)
 	{
@@ -132,23 +126,47 @@ void conflict_resolve_forgetabout_sharedmemory(uint32_t* conflict_color, // Arra
 	}
 	std::cout<<std::endl;
 
+	int choseL = 0;
+	int counter = 1;
 	neighLen = neighLen1;
 	int theColor = colorID;
 	void  *d_temp_storage = NULL;
     	size_t    temp_storage_bytes = 0;
-	DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, sizeNode);
-		cudaDeviceSynchronize();
+	DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, sizeNode+1);
 	HANDLE_ERROR(cudaMalloc(&d_temp_storage, temp_storage_bytes));
-	int choseL = 0;
-	int counter = 1;
+
+//	//test
+//	uint32_t *test(NULL), *test_out(NULL);
+//	HANDLE_ERROR(cudaMallocManaged(&test, 10*sizeof(uint32_t)));
+//	HANDLE_ERROR(cudaMallocManaged(&test_out, 11*sizeof(uint32_t)));
+//	for(int i=0; i<10; i++)
+//		test[i]=i;
+//
+//	DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, 3);
+//	HANDLE_ERROR(cudaMalloc(&d_temp_storage, temp_storage_bytes));
+//	DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, 3);
+//	cudaDeviceSynchronize();
+//	for(int i=0; i<2; i++)
+//		std::cout<<" "<<neighLen[i]<<" ";
+//	std::cout<<std::endl;
+//
+//	for(int i=0; i<3; i++)
+//		std::cout<<" "<<scanArray[i]<<" ";
+//	
+//	std::cout<<std::endl;
 	
 	while(true) {
-
-		DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, sizeNode);			
-		cudaDeviceSynchronize();
-		for(int i=0; i<=sizeNode; i++)
+		for(int i=0; i<sizeNode; i++)
 		{
-			std::cout<<"scan_neighLen["<<i<<"]= "<<neighLen[i]<<"   ";
+			std::cout<<"neighLen["<<i<<"]= "<<neighLen[i]<<"  ";
+		}
+		std::cout<<std::endl;
+
+		DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, neighLen, scanArray, sizeNode+1);			
+		cudaDeviceSynchronize();
+		for(int i=0; i<sizeNode+1; i++)
+		{
+			std::cout<<"scan_neighLen["<<i<<"]= "<<scanArray[i]<<"   ";
 		}
 		std::cout<<std::endl;
 		
@@ -159,9 +177,21 @@ void conflict_resolve_forgetabout_sharedmemory(uint32_t* conflict_color, // Arra
 		cudaDeviceSynchronize();
 		theColor = numColor+counter;
 
-		DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, changeColor, scanArray, sizeNode);	
+		DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, changeColor, scanArray, sizeNode+1);	
 		cudaDeviceSynchronize();
+
+		for(int i=0; i<sizeNode; i++)
+		{
+			std::cout<<"changeColor["<<i<<"]= "<<changeColor[i]<<"  ";
+		}
+		std::cout<<std::endl;	
 		
+		for(int i=0; i<sizeNode+1; i++)
+		{
+			std::cout<<"scan_changeColor["<<i<<"]= "<<scanArray[i]<<"   ";
+		}
+		std::cout<<std::endl;
+
 		choseL = choseL^1;
 		if(choseL == 1)
 		{
@@ -174,12 +204,24 @@ void conflict_resolve_forgetabout_sharedmemory(uint32_t* conflict_color, // Arra
 			newNodes = nodes1;
 		}
 	
-		int check = 0;
-		GenNewNodes<<<1,32>>>(nodes, newNodes, neighLen, newNeighLen, sizeNode, scanArray, check);
+		std::cout<<"sizeNode: "<< sizeNode<<std::endl;
+		GenNewNodes<<<1,32>>>(nodes, newNodes, neighLen, newNeighLen, sizeNode, scanArray);
 		cudaDeviceSynchronize();
-		if(check == 1) break;
+		sizeNode = scanArray[sizeNode];
+		if(sizeNode == 0) break;
+
+		printf("new sizeNode: %d\n", sizeNode);
 		nodes = newNodes;
+		for(int i=0; i<sizeNode; i++)
+		{
+			std::cout<<"newNode["<<i<<"]=  "<<nodes[i]<<"   ";
+		}
+		std::cout<<std::endl;
 		neighLen = newNeighLen;
+		for(int i=0; i<sizeNode; i++)
+		{
+			std::cout<<"newNeighLen["<<i<<"]=  "<<neighLen[i]<<"   ";
+		}
 		std::cout<<"counter: "<<counter<<std::endl;
 		counter++;
 	}	
